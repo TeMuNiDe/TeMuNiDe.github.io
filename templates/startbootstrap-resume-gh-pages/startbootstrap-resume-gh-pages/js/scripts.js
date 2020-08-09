ab31791ee97755edfe1bf8899b8c0ae4451d3be4
TeMuNiDe <varma.vihaari@gmail.com>
2020-08-09 21:57:59 +0530

    Bootstrap Theme Updated

---
+++ templates/startbootstrap-resume-gh-pages/startbootstrap-resume-gh-pages/js/scripts.js
@@ -0,0 +1,49 @@
+/*!
+    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+    */
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#sideNav",
+    });
+})(jQuery); // End of use strict
+
+var options = {
+fallbackLink: "<p>This is a <a href='https://temunide.github.io/awards/award_of_excellence.pdf'>View Award</a></p>",
+height: "500px",
+pdfOpenParams: { view: 'FitV',page: "1" }
+};
+PDFObject.embed("../../../awards/award_of_excellence.pdf", "#excel_pdf");
